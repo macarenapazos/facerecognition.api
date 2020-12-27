fb9cd9fc93d3d1bf0f0aa5773b747317101fc369
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-12-27 13:44:33 -0300

    actualizando db

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-perpendicular-03932',
-    user : 'macupazos',
-    password : '29101993macu',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
