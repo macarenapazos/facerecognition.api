4a3b7c81df4b0ae68ed77d9638eb7435bebdbdc2
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-12-28 10:07:41 -0300

    actualizando

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,26 @@
-const clarifai = require('clarifai');
+const Clarifai = require('clarifai');
 
+//You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
  apiKey: '14a18267376347ada27e9f4cb8b7dc70'
 });
 
 const handleApiCall = (req, res) => {
   app.models
-  .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .then(data => {
-  	res.json(data);
-  })
-  .catch(err => res.status(400).json('unable to work with API'))
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    // to:
+    // .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
 const handleImage = (req, res, db) => {
@@ -25,6 +35,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-	handleImage,
-	handleApiCall
+  handleImage,
+  handleApiCall
 }
