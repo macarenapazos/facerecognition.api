b8ffb96d09697b69e89a6dcd822202b269387805
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-12-28 11:11:17 -0300

    actualizando database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'macupazos',
-    password : '29101993macu',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
